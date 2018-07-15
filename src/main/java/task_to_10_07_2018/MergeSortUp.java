002dd8269236433aa50d22ba748fce77aed822e0
Dmitry Kropotov <Dmitry_Kropotov@epam.com>
2018-07-15 22:20:14 +0300

    I removed null as special element, made algorithm better and added checking on the case, if there are nulls in the array and refactored code following "clean code rules"

--- src/main/java/task_to_10_07_2018/MergeSortUp.java
+++ src/main/java/task_to_10_07_2018/MergeSortUp.java
@@ -3,6 +3,7 @@ package task_to_10_07_2018;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class MergeSortUp {
 
@@ -11,6 +12,12 @@ public class MergeSortUp {
         mergeSortUp(strings);
         List<String> listOfStrings = Arrays.asList(strings);
         listOfStrings.forEach(System.out::print);
+        System.out.println();
+        String[] strings2 = {null, "Croatia", null, "England", "Ronaldo", null};
+        mergeSortUp(strings2);
+        listOfStrings = Arrays.asList(strings2);
+        listOfStrings.forEach(System.out::print);
+        System.out.println();
         Integer[] ints = {10, 24, 12, 5, 43, 30};
         mergeSortUp(ints);
         List<Integer> listOfIntegers = Arrays.asList(ints);
@@ -18,11 +25,12 @@ public class MergeSortUp {
     }
 
     private static <T extends Comparable<T>> void mergeSortUp(T[] array) {
-        if (array.length <= 1) {
+        if (array.length <= 1 || Arrays.stream(array).allMatch(Objects::isNull)) {
             return;
         }
-        T[] leftArray = (T[])Array.newInstance(array[0].getClass(), array.length/2);
-        T[] rightArray = (T[])Array.newInstance(array[0].getClass(), array.length == Integer.MAX_VALUE ? leftArray.length + 1
+        T notNull = Arrays.stream(array).filter(Objects::nonNull).findFirst().get();
+        T[] leftArray = (T[])Array.newInstance(notNull.getClass(), array.length/2);
+        T[] rightArray = (T[])Array.newInstance(notNull.getClass(), array.length == Integer.MAX_VALUE ? leftArray.length + 1
         : (array.length+1)/2);
         System.arraycopy(array, 0, leftArray, 0, leftArray.length);
         System.arraycopy(array, array.length/2, rightArray, 0, rightArray.length);
@@ -31,18 +39,37 @@ public class MergeSortUp {
         //merging left and right arrays
         int indexOfElementFromLeftArrayToMerge = 0;
         int indexOfElementFromRightArrayToMerge = 0;
+        T elementFromLeftArrayToMerge = null;
+        T elementFromRightArrayToMerge = null;
         for (int i = 0; i < array.length; ++i) {
-            T elementFromLeftArrayToMerge = indexOfElementFromLeftArrayToMerge < leftArray.length ?
-                    leftArray[indexOfElementFromLeftArrayToMerge] : null;
-            T elementFromRightArrayToMerge = indexOfElementFromRightArrayToMerge < rightArray.length ?
-                    rightArray[indexOfElementFromRightArrayToMerge] : null;
-            if(elementFromRightArrayToMerge == null || elementFromLeftArrayToMerge != null && elementFromLeftArrayToMerge.compareTo(elementFromRightArrayToMerge) <= 0) {
-               array[i] = elementFromLeftArrayToMerge;
-               indexOfElementFromLeftArrayToMerge++;
+            if (indexOfElementFromLeftArrayToMerge < leftArray.length) {
+                elementFromLeftArrayToMerge = leftArray[indexOfElementFromLeftArrayToMerge];
+            }
+            if (indexOfElementFromRightArrayToMerge < rightArray.length) {
+                elementFromRightArrayToMerge = rightArray[indexOfElementFromRightArrayToMerge];
+            }
+            //If there is null element in at least one of the arrays leftArray or rightArray, we put it in the end of array
+
+            //I tried to make the "if" statement shorter following Clean Code rule "if statement should be one line long" and created two separated functions ifLeftIndexIsNotInTheEndOfArray and bothElementsNotNull and one more addValueToArrayAndIncreaseValue, because in "if" statement should be only one command
+            if(ifLeftIndexIsNotInTheEndOfArray(indexOfElementFromLeftArrayToMerge, leftArray.length, elementFromLeftArrayToMerge, elementFromRightArrayToMerge) || indexOfElementFromRightArrayToMerge == rightArray.length) {
+               indexOfElementFromLeftArrayToMerge = addValueToArrayAndIncreaseValue(array, i, elementFromLeftArrayToMerge, indexOfElementFromLeftArrayToMerge);
             } else {
-               array[i] = elementFromRightArrayToMerge;
-               indexOfElementFromRightArrayToMerge++;
+               indexOfElementFromRightArrayToMerge = addValueToArrayAndIncreaseValue(array, i, elementFromRightArrayToMerge, indexOfElementFromRightArrayToMerge);
             }
         }
+    }
+
+    private static<T extends Comparable<T>> boolean ifLeftIndexIsNotInTheEndOfArray(int leftIndex, int lengthOfFirstElement, T firstElement, T secondElement) {
+        return leftIndex < lengthOfFirstElement &&
+               (bothElementsNotNull(firstElement, secondElement) && firstElement.compareTo(secondElement) <= 0
+               || secondElement == null);
+    }
+    private static<T> boolean bothElementsNotNull(T firstElement, T secondElement) {
+        return firstElement != null && secondElement != null;
+    }
+
+    private static<T> int addValueToArrayAndIncreaseValue(T[] array, int indexToAdd, T elementToAdd, int indexOfElement) {
+        array[indexToAdd] = elementToAdd;
+        return indexOfElement + 1;
     }
 }
